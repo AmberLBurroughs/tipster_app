b4cee46ed40c7f3298c9e135602fc06cb6fa9815
jwong1219 <jwong1219@gmail.com>
2017-12-03 17:44:10 -0800

    finished the log in and sign up helper funcions. going to start work on the submit tip helper function

--- client/src/Utils/helper.js
+++ client/src/Utils/helper.js
@@ -9,7 +9,35 @@ export default{
 			password: loginState.password
 		};
 		axios.post('/login', user)
-		.then( response => console.log(response));
-	}
+		.then(response => console.log(response));
+	},
+
+  signUpHelper: (signUpState) => {
+    if (!signUpState.match) {
+      console.log(`passwords do not match`)
+    }
+    else {
+      console.log(`passwords match, sending user info`);
+      let newUser = {
+        email: signUpState.email,
+        password: signUpState.password
+      };
+      axios.post('/signup', newUser)
+      .then(response => console.log(response));
+    }
+  },
+
+  tipHelper: (token) => {
+    let transaction = {
+      location: this.props.state.markerClicked.id,
+      amount: this.state.amount,
+      anonymous: this.state.anonymous,
+      note: this.state.note,
+      token: token
+    }
+    axios.post('/tip', transaction)
+    .then();
+
+  }
 
 }
